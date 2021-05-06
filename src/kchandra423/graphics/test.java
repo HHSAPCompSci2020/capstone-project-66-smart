728f205540b849494ac39e84d5ecf93d709c99e5
Don't Do Drugs <spencerye425@gmail.com>
2021-05-06 13:27:54 -0700

    Alpha Release

--- src/kchandra423/graphics/test.java
+++ src/kchandra423/graphics/test.java
@@ -1,7 +1,9 @@
 package kchandra423.graphics;
 
-public class test {
-    public static void main (String[] args){
-    	System.out.println("yea");
+public class test 
+{
+    public static void main (String[] args)
+    {
+    	System.out.println("small change so we can title something alpha release");
     }
 }
