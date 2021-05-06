1ac064e4f26f6d1e6034e8825511706f6e5ca8ec
Kumar Chandra <kchandra423@student.fuhsd.org>
2021-05-06 13:37:39 -0700

    commented out unused classes

--- src/kchandra423/graphics/test.java
+++ src/kchandra423/graphics/test.java
@@ -1,9 +1,9 @@
-package kchandra423.graphics;
-
-public class test 
-{
-    public static void main (String[] args)
-    {
-    	System.out.println("small change so we can title something alpha release");
-    }
-}
+//package kchandra423.graphics;
+//
+//public class test
+//{
+//    public static void main (String[] args)
+//    {
+//    	System.out.println("small change so we can title something alpha release");
+//    }
+//}
