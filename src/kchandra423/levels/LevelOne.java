cb1b9843ae923301473129e8103fce0c4d1e8a88
Don't Do Drugs <spencerye425@gmail.com>
2021-05-07 22:40:21 -0700

    Create basic first level outlines

---
+++ src/kchandra423/levels/LevelOne.java
@@ -0,0 +1,14 @@
+package kchandra423.levels;
+
+/**
+ * A class that represents the first level of the game
+ * @author Spencer Ye
+ * @version 0.0.1
+ */
+public class LevelOne 
+{
+	public LevelOne(String characterName)
+	{
+		
+	}
+}
---
+++ src/kchandra423/levels/LevelThree.java
@@ -0,0 +1,11 @@
+package kchandra423.levels;
+
+/**
+ * A class that represents the third level of the game
+ * @author Spencer Ye
+ * @version 0.0.0
+ */
+public class LevelThree 
+{
+
+}
