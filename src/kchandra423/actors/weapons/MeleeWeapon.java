89414a3f9ee7d414621d2122a2646c03f4548911
Kumar Chandra <kchandra423@student.fuhsd.org>
2021-05-16 01:06:08 -0700

    Update MeleeWeapon.java

--- src/kchandra423/actors/weapons/MeleeWeapon.java
+++ src/kchandra423/actors/weapons/MeleeWeapon.java
@@ -8,7 +8,7 @@ import kchandra423.graphics.textures.KImage;
 import kchandra423.levels.Room;
 
 public class MeleeWeapon extends Weapon {
-    private final long swingTime = 1;
+    private final float swingTime = 0.5f;
     private long swingStartTime = System.currentTimeMillis();
 
     /**
@@ -24,8 +24,8 @@ public class MeleeWeapon extends Weapon {
     public void act(DrawingSurface d, Room room) {
         //if you cannot fire, it means that you are firing
         if (!canFire()) {
-            float differential = (float) (((System.currentTimeMillis() - swingStartTime) / (float) (swingTime * 1000)) * Math.PI * 2);
-            image.rotate((float) (Math.PI / 4 * Math.sin(differential)));
+            float differential = (float) (((System.currentTimeMillis() - swingStartTime) /  (swingTime * 1000)) * Math.PI /2* 3 +Math.PI);
+            image.rotate((float) (Math.PI / 4 * Math.cos(differential)));
             for (Enemy e :
                     room.getEnemies()) {
                 if (intersects(e)) {
